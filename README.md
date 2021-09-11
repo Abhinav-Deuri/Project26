bc9b40680a779f149a8f5f2d28c10323d2bcc401
Abhinav-Deuri <88224337+Abhinav-Deuri@users.noreply.github.com>
2021-09-11 10:19:59 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project Template 26
+# project-C26-template
--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var canvas;
-var palyer, playerBase;
-var computer, computerBase;
-
-//Declare an array for arrows playerArrows = [ ]
+var palyer, playerBase, playerArcher;
 var playerArrows = [];
-var arrow;
-
-function preload(){
-  backgroundImg = loadImage("assets/background.gif")
+var board1
+var board2
+var boardno = []
+var limit = 10
+var shooted 
+function preload() {
+  backgroundImg = loadImage("./assets/background2.gif");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
 }
-
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
@@ -23,79 +24,127 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
-  playerArcher = new PlayerArcher(340, playerBase.body.position.y - 180, 120, 120);
- 
-  computerBase = new ComputerBase(width - 300, random(450, height - 300),180,150);
-  computer = new Computer(width - 280, computerBase.body.position.y - 153, 50, 180);
-  computerArcher = new ComputerArcher(width - 340, computerBase.body.position.y - 180, 120, 120);
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.position.y - 112,
+    120,
+    120
+  );
+
+  board1 =  new Board(width-300,250,50,200)
+  board2 =  new Board(width-400,height-325,50,200)
 }
 
 function draw() {
   background(backgroundImg);
 
   Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
 
+  playerArcher.display();
+  board1.display()
+  board2.display()
+
+  for (var i = 0; i < playerArrows.length; i++) {
+    if (playerArrows[i] !== undefined) {
+      playerArrows[i].display();
+      collisionWithBoard1(i)
+      collisionWithBoard2(i)
+
+    }
+  }
+
+  if(shooted === 1&&shooted!==2){
+    text("You shooted Board 1!",width - width/6,75)
+  }
+  if(shooted === 2&&shooted!==1){
+    text("You shooted Board 2!",width - width/6,150)
+  }
+
+  if(shooted ===3){
+    text("You shooted Both-Boards!",width - width/6,100)
+  }
   // Title
   fill("#FFFF");
   textAlign("center");
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
-
- 
-  playerBase.display();
-  player.display();
-  
-
-  computerBase.display();
-  computer.display();
-  
-  playerArcher.display();
-  computerArcher.display()
-
- // Use for loop to display arrow using showArrow() function
- for (var i = 0; i < playerArrows.length; i++) {
-  showArrows(i, playerArrows);
-}
-
+  text("Arrows left:"+limit+"/10",width / 8,100)
 }
 
 function keyPressed() {
-
-  if(keyCode === 32){
-    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+  if (keyCode === 32 & limit >0) {
+    limit-=1
     var posX = playerArcher.body.position.x;
     var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle+PI/2;
+    var angle = playerArcher.body.angle;
+    //console.log(angle);
 
-    var arrow = new PlayerArrow(posX, posY, 100, 10);
+    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
 
-    arrow.trajectory = [];
     Matter.Body.setAngle(arrow.body, angle);
     playerArrows.push(arrow);
-
   }
 }
 
-function keyReleased () {
-
-  if(keyCode === 32){
-    //call shoot() function for each arrow in an array playerArrows
+function keyReleased() {
+  if (keyCode === 32) {
     if (playerArrows.length) {
-      var angle = playerArcher.body.angle+PI/2;
+      var angle = playerArcher.body.angle;
       playerArrows[playerArrows.length - 1].shoot(angle);
     }
   }
-
 }
-//Display arrow and Tranjectory
-function showArrows(index, arrows) {
-  arrows[index].display();
-  
+                                                                                       
+
+
+function collisionWithBoard1(index){
+  if (playerArrows[index]!==undefined&&board1!==undefined){
+    var collision1 = Matter.SAT.collides(playerArrows[index].body, board1.body);
+    if (collision1.collided){
+      
+      Matter.World.remove(world, playerArrows[index].body);
+      delete playerArrows[index]
+      
+      if(shooted===2){
+        shooted =3
+      }else if(shooted!==5125&&shooted!==3){
+        shooted = 1
+      }
+      
+    }
+
+  }
     
-  
- 
-
 }
 
+function collisionWithBoard2(index){
+  if (playerArrows[index]!==undefined&&board2!==undefined){
+    var collision2 = Matter.SAT.collides(playerArrows[index].body, board2.body);
+    if (collision2.collided){
+      Matter.World.remove(world, playerArrows[index].body);
+      delete playerArrows[index]
+      
+      if(shooted===1){
+        shooted = 3
+      }else if(shooted!==5125&&shooted!==3){
+        shooted=2
+      }
+    }
+
+  }
+    
+}
